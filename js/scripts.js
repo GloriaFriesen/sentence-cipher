3471c3a168cc1404868ffc46820b6219aee2c817
Gloria Friesen and Liam Stabeno <liam@>
2017-02-13 15:17:53 -0800

    create initial scripts.js, index.html, and import the jQuery file..

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+
+var sentence = prompt("Enter a sentence...");
+
+var firstLetter = sentence.charAt(0);
+
+var firstUpper = firstLetter.toUpperCase();
+
+    alert(firstUpper);
+
+var lastLetter = sentence.charAt(sentence.length-1);
+
+var lastUpper = lastLetter.toUpperCase();
+
+    alert(lastUpper);
+
+    
+
+
+
+
+// var firstLetter = function()
+//
+// var lastLetter
